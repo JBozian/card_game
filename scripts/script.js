f3094972e028a5c5507b12b35012254e9b048639
Martin Marks <martinm29@gmail.com>
2025-03-14 00:36:38 -0400

    introduce deck class

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-// let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-// let suits = ["♠", "♥", "♦", "♣"];
-// let deck = [];
-
-
-
 class Card {
     constructor(value, suit) {
         this.value = value;
@@ -15,54 +9,50 @@ class Card {
     }
 }
 
-
 class Deck {
     constructor() {
         this.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
         this.suits = ["♠", "♥", "♦", "♣"];
-        this.deck = []
+        this.pack = this.createPack();
     }
 
-     createDeck() {
+     createPack() {
+        let temp = [];
         for (let i = 0; i < this.values.length; i++) {
             for (let j = 0; j < this.suits.length; j++) {
                 // deck.push({ suit: suits[i], value: values[j] })
                 let card = new Card(this.values[i], this.suits[j]);
-                deck.push(card);
+                temp.push(card);
             }
         }
+        return temp;
+    }
+
+    shuffle() {
+        for (let i = this.pack.length - 1; i >= 1; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.pack[i], this.pack[j]] = [this.pack[j], this.pack[i]];
+        }
+    }
+
+    getCard() {
+        return this.pack.pop();
     }
 }
 
-
-
-
-function shuffle() {
-    for (let i = deck.length - 1; i >= 1; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-}
-
-
-createDeck();
-shuffle();
-console.table(deck);
-
+let deck = new Deck();
+deck.shuffle();
+console.table(deck.pack);
 
 let drawBtn = document.getElementById("draw-btn");
-
 let deckElement = document.getElementById("deck");
 
-
 function drawCard() {
-    const card = deck.pop();
+    const card = deck.getCard();
     if (card)
         deckElement.textContent = card.combo();
     else
         deckElement.textContent = "No more cards!";
 }
-
-
 
 drawBtn.addEventListener("click", drawCard);
